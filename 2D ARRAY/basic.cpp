9c1b024a3e1675642d224b16cfbe08e8a72ea7a8
Abhijit Mondal <abhijitamp378@gmail.com>
2025-07-14 11:01:47 +0530

    2d

---
+++ 2D ARRAY/basic.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int student[3][3]={{100,95,94},
+                       {85,76,98},
+                       {98,65,89}};
+
+cout <<student[3][3]<<endl;
+return 0;
+}                      
---
+++ 2D ARRAY/input_output.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Enter rows and columns: ";
+    cin >> n >> m;
+
+    int arr[n][m]; 
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
