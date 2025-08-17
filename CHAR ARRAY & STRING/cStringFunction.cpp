29d14894d3556a2e79f906f9da4dbd0ff2c35b6d
Abhijit Mondal <abhijitamp378@gmail.com>
2025-08-17 10:14:39 +0530

    concatenate(input) {   return input.join('');

--- CHAR ARRAY & STRING/cStringFunction.cpp
+++ CHAR ARRAY & STRING/cStringFunction.cpp
@@ -12,4 +12,17 @@ void copyString (char*a1, char*a2) {
     a2[i] = '\0'; // null-terminate the copied string
 }
 
-//
+// to concatenate strings
+void concatenateStrings (char*a1, const char*a2) {
+    int i = 0;
+    while (a1[i] != '\0') {
+        i++;
+    }
+    int j = 0;
+    while (a2[j] != '\0') {
+        a1[i] = a2[j];
+        i++;
+        j++;
+    }
+    a1[i] = '\0'; // null-terminate the concatenated string
+}
