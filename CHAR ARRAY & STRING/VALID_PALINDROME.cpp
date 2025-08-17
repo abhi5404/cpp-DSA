f83deec456e1c2720af61be6e06dfc8cfdb36fc7
Abhijit Mondal <abhijitamp378@gmail.com>
2025-08-17 10:02:01 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ CHAR ARRAY & STRING/VALID_PALINDROME.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+bool isPalindrome(string str){
+    int st=0;
+    int end = str.length()-1;
+    while(st<end){
+        if(str[st]!=str[end]){
+            return false;
+        }
+        st++;
+        end--;
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    if (isPalindrome(str)) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+
+    return 0;
+}
