b717f07cea395fac36eb7165609663e7d0e354db
Abhijit Mondal <abhijitamp378@gmail.com>
2025-08-17 11:24:57 +0530

    valid anagram using sorting

--- CHAR ARRAY & STRING/VALID_ANAGRAM.cpp
+++ CHAR ARRAY & STRING/VALID_ANAGRAM.cpp
@@ -1,1 +1,35 @@
-// valid anagram 
+// valid anagram using sorting
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isAnagram(string str1, string str2) {
+    if (str1.length() != str2.length()) {
+        return false;
+    }
+
+    // Sort both strings
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+
+    // Compare sorted strings
+    return str1 == str2;
+}
+
+int main() {
+    string str1, str2;
+    cout << "Enter first string: ";
+    cin >> str1;
+    cout << "Enter second string: ";
+    cin >> str2;
+
+    if (isAnagram(str1, str2)) {
+        cout << "The strings are anagrams." << endl;
+    } else {
+        cout << "The strings are not anagrams." << endl;
+    }
+
+    return 0;
+}
+
+// time complexity 
