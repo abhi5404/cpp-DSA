e470216dddd3891428c9eb1c8c67b651471564ac
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-08 21:28:07 +0530

    Implement optimized bubble sort with early exit condition and update comments for clarity

--- SORTING/bubbleSort.cpp
+++ SORTING/bubbleSort.cpp
@@ -5,10 +5,27 @@ int main (){
     int arr[]={64,34,25,12,22,11,90};
     int n= sizeof(arr)/sizeof(arr[0]);
     
-    for(int i=0;i<n-1;i++){  //outer loop (n-1 passes)
+    // for(int i=0;i<n-1;i++){  //outer loop (n-1 passes)
         
+    //      //inner loop (for traversing unsorted array)
+
+    //     for(int j=0;j<n-i-1;j++){  
+    //         if(arr[j]>arr[j+1]){
+    //             //swap
+    //             // int temp=arr[j];
+    //             // arr[j]=arr[j+1];
+    //             // arr[j+1]=temp;
+    //             swap(arr[j],arr[j+1]);
+    //         }
+    //     }
+    // }
+
+
+    //optimized bubble sort
+    for(int i=0;i<n-1;i++){  //outer loop (n-1 passes)
+        bool flag=false;
          //inner loop (for traversing unsorted array)
-         
+
         for(int j=0;j<n-i-1;j++){  
             if(arr[j]>arr[j+1]){
                 //swap
@@ -16,7 +33,11 @@ int main (){
                 // arr[j]=arr[j+1];
                 // arr[j+1]=temp;
                 swap(arr[j],arr[j+1]);
+                flag=true;
             }
+        }
+        if(flag==false){
+            break; //array is already sorted
         }
     }
 
@@ -28,7 +49,7 @@ int main (){
 }
 
 
-//time complexity: O(n^2)
+
 
 // short algorithm for remembering this sort is given below :
 // begin : for i=0 to n-1(outer loop)
@@ -36,3 +57,5 @@ int main (){
 // if arr[j]>arr[j+1] then swap(arr[j],arr[j+1])
 // end inner loop
 // end outer loop
+
+// Time Complexity: O(n^2) in worst and average case, O(n) in best case (when array is already sorted)
