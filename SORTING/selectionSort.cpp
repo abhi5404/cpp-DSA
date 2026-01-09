17ccc1dd29ba2153841870c22609cffd420bf547
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-09 11:40:15 +0530

    Refactor comments for clarity in selection sort implementation

--- SORTING/selectionSort.cpp
+++ SORTING/selectionSort.cpp
@@ -4,9 +4,8 @@ int main (){
     int arr[]={64,25,12,22,11};
     int n= sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n-1;i++){  //outer loop (n-1 passes)
-        int min_index=i;
-         //inner loop (for traversing unsorted array)
-
+        int min_index=i;      // unsorted part starts
+        
         for(int j=i+1;j<n;j++){  
             if(arr[j]<arr[min_index]){
                 min_index=j;
