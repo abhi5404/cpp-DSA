1285b59d67f2e0f2af6df8e6793df29ce38b47e3
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-12 19:41:58 +0530

    Refactor code structure for improved readability and maintainability

---
+++ SEARCHING/LowerBound.cpp
@@ -0,0 +1,15 @@
+// using linear search to find lower bound
+
+#include <iostream>
+using namespace std;
+int main (){
+    int arr[]={1,2,4,5,9,15,18,21,24};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    int key=16;
+    for (int i=0;i<n;i++){
+        if(arr[i]>=key){
+            cout<<"Lower bound of "<<key<<" is "<<arr[i-1]<<endl;
+            return 0;
+    }
+}
+}
